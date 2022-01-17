d86c7b7cdf1b5ede8bb8391d00551fb7d1ad12e9
unknown <kellskelechi@gmail.com>
2022-01-17 11:12:12 +0100

    market-place

--- src/components/pages/Market.js
+++ src/components/pages/Market.js
@@ -5,7 +5,16 @@ import React from 'react'
 
     return (
         <>
-          HOme  
+      <div className="market-container">
+     <div className="intro">
+      <h2>Introducing YESPORTS</h2>
+      <h1> Get closer to your Favourite<br/>
+      esports team</h1>
+      <p>All YESPORTS NFTs grant access<br/>
+      to the metaverse</p>
+     </div>
+ <button>Veiw pack</button>
+      </div>
         </>
     )
 }
